875a3e859b366da4d6ad4dd27967ddc7b4025c53
M-Muhaddis-Irshad <muhaddisirshad58@gmail.com>
2025-11-25 20:06:23 +0500

    Add Arrow svg's
    For Increament & Decreament (Counter)

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,9 +1,18 @@
 import './Header.css'
 
-const header = () => {
+const Header = () => {
     return (
         <>
-        
+            <div className="logoNameCntnr">
+                <a href="">
+                    <img src="" alt="" />
+                </a>
+                <span className='logoName'>
+                    Vite Counter App
+                </span>
+            </div>
         </>
     )
 }
+
+export default Header
